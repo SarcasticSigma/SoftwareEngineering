60b8ea42c2b91c6184b716f8ed7c3f14cf54aa3c
SarcasticSigma <sarcasticsigma@gmail.com>
2022-11-09 19:16:01 -0500

    Worked on updating test cases, added some scaffolding to SoftwareEngineeringProject.py, worked on clarifying written test cases.

---
+++ SoftwareEngineeringProject.py
@@ -0,0 +1,26 @@
+class RetirementCalculator:
+    def __init__(self):
+        self.employee = None
+        self.totalContributions = None
+        self.companyContribution = None
+        self.employeeContribution = None
+        self.paidToEmployee = None
+        self.hourlyPercentage = None
+        self.hoursWorked = None
+        self.retirementPlanList = [1,4,56,77]
+        self.planListSelection = None
+        self.employeeName = None
+        self.hourlyRate = None
+        self.startMenuOptions = None
+        self.startSelection = None
+
+    def getEmployeeData(self):
+        pass
+
+    def makeEmployee(self, param):
+        #Employee(param)
+        #Employee.setPlan(param)
+        pass
+
+    def readPlans(self):
+        pass
